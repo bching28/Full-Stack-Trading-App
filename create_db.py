963a57be2f870272746f25990f76666f8c6b572e
Bryan Ching <bryanching7373@gmail.com>
2021-06-13 23:25:12 -0700

    add code for web app strategy

--- create_db.py
+++ create_db.py
@@ -9,7 +9,8 @@ cursor.execute("""
     CREATE TABLE IF NOT EXISTS stock (
         id INTEGER PRIMARY KEY,
         symbol TEXT NOT NULL UNIQUE,
-        company TEXT NOT NULL
+        name TEXT NOT NULL,
+        exchange TEXT NOT NULL
     )
 """)
 
@@ -22,10 +23,31 @@ cursor.execute("""
         high NOT NULL,
         low NOT NULL,
         close NOT NULL,
-        adjusted_close NOT NULL,
         volume NOT NULL,
         FOREIGN KEY (stock_id) REFERENCES stock (id)
     )
 """)
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS strategy (
+        id INTEGER PRIMARY KEY,
+        name NOT NULL
+    )
+""")
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS stock_strategy (
+        stock_id INTEGER NOT NULL,
+        strategy_id INTEGER NOT NULL,
+        FOREIGN KEY (stock_id) REFERENCES stock (id)
+        FOREIGN KEY (strategy_id) REFERENCES strategy (id)
+    )
+""")
+
+strategies = ['opening_range_breakout', 'opening_range_breakdown']
+for strategy in strategies:
+    cursor.execute("""
+        INSERT INTO strategy (name) VALUES (?)
+    """, (strategy,))
 
 connection.commit()
