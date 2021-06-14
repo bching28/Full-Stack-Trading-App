f6a917e0cdfc21f5d21ba264f70a18924fe44e54
Bryan Ching <bryanching7373@gmail.com>
2021-06-13 23:20:57 -0700

    add script for populating stock info per company

--- populate_stocks.py
+++ populate_stocks.py
@@ -9,7 +9,7 @@ connection.row_factory = sqlite3.Row
 cursor = connection.cursor()
 
 cursor.execute("""
-    SELECT symbol, company FROM stock
+    SELECT symbol, name FROM stock
 """)
 
 rows = cursor.fetchall()
@@ -18,19 +18,12 @@ symbols = [row['symbol'] for row in rows]
 api = tradeapi.REST(API_KEY, SECRET_KEY, base_url=BASE_URL)
 assets = api.list_assets()
 
-# for asset in assets:
-#     try:
-#         if (asset.status == 'active' and asset.tradable):
-#             cursor.execute("INSERT INTO stock (symbol, company) VALUES (?, ?)", (asset.symbol, asset.name))
-#     except Exception as e:
-#         print(asset.symbol)
-#         print(e)
-
 for asset in assets:
     try:
         if asset.symbol not in symbols and asset.status == 'active' and asset.tradable:
             print(f"Added a new stock {asset.symbol} {asset.name}")
-            cursor.execute("INSERT INTO stock (symbol, company) VALUES (?, ?)", (asset.symbol, asset.name))
+            cursor.execute("INSERT INTO stock (symbol, name, exchange) VALUES (?, ?, ?)",
+                           (asset.symbol, asset.name, asset.exchange))
     except Exception as e:
         print(asset.symbol)
         print(e)
